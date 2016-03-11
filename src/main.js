7ca691e532e014fcb5d7b9a65338bd5b9c192c5e
j-borges <joshuaborges@gmail.com>
2016-03-11 11:03:02 -0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,75 @@
+import express from 'express'
+import pkg from '../package.json'
+import faker from 'faker'
+import _ from 'lodash'
+import bodyParse from 'body-parser'
+
+const users = _.times(100, () => {
+    return{
+        firstName: faker.name.firstName(),
+        lasName: faker.name.lastName(),
+        age: _.random(18, 99)
+    }
+})
+
+const app = express()
+
+app
+    .use(bodyParser.json())
+
+    .use((req, res, next) => {
+        const reqData = {
+            path: req.path,
+            query: req.query,
+            body: req.body,
+        }
+        console.log('REQUEST:\n' , JSON.stringify (reqDate, null, 2))
+            next()
+    })
+
+    .get('/', (req, res) => {
+    res.send('you are at root!')
+
+    .get('/users', (req, res) => {
+        let filteredUsers = [...users]
+
+        if (req.query.ageMin)
+            filteredUsers = filteredUsers
+                .filter((user) => user.age >= req.query.ageMin)
+        }
+        if (req.query.ageMax)
+            filteredUsers = filteredUsers
+                .filter((user) => user.age >= req.query.ageMax)
+         if (req.query.sortBy)
+                filteredUsers = _.sortBy(filteredUsers, req.query.sortBy)
+
+
+          res.json(filteredUsers)
+    })
+
+        .post('/users', (req, res) => {
+            users.push(req.body)
+            res.json(req.body)
+     })
+
+.get('/package', (req, res) => {
+    res.json(pkg)
+})
+
+.get('/quoe', (req, res) => {
+    res.json ({
+        author: faker.name.findname(),
+        quote: faker.hacker.phrase()
+    })
+})
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, () => {
+    console.log(`Express app listening on port ${PORT}`)
+})
+
+
+
+
